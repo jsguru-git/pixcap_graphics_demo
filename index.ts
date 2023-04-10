6c6f2e9b95c8ba463cf9d2326efb86679ef3db15
JSGURU <js.guru@hotmail.com>
2023-04-11 00:09:08 +0300

    customize cylinder

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder, Quaternion  } from 'babylonjs';
-import * as GUI from 'babylonjs-gui';
+import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder, Quaternion, AbstractMesh, Nullable  } from 'babylonjs';
 import 'babylonjs-loaders';
 
 const canvas = document.getElementById("canvas");
@@ -20,24 +19,37 @@ function windowOnClick(event: any) {
 
 closeButton?.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
-var meshContent = {
-	Plane: 'This is extremely nice BOX! <br/> <p style = "color:green">No problems with CSS styling.</p>',
-	IcoSphere: 'This SPHERE is really wonderful! <br/><h3>HTML tags allowed :)</h3>',
-	Cylinder: `<div class="modal-title">Customize Cylinder</div>
-	<div class="modal-body">
-		<label for="diameter">Diameter: </label><br/>
-		<input type="range" id="diameter" value="1" min="0.1" max="2.0" step="0.1" oninput="diameterVal.value = this.value" />
-		<output id="diameterVal">1</output><br/>
-		<label for="height">Height: </label><br/>
-		<input type="range" id="height" value="1" min="0.1" max="2.0" step="0.1" oninput="heightVal.value = this.value" />
-		<output id="heightVal">1</output>
-	</div>`
-}
 
 const engine = new Engine(canvas, true, {});
 const scene = new Scene(engine);
+var currentMesh: Nullable<AbstractMesh>;
+var cylinderHeight = 2;
+var cylinderDiameter = 1;
+
+var meshContent = {
+	Plane: 'This is extremely nice BOX! <br/> <p style = "color:green">No problems with CSS styling.</p>',
+	IcoSphere: 'This SPHERE is really wonderful! <br/><h3>HTML tags allowed :)</h3>',
+	Cylinder: `
+	<div class="modal-title">Customize Cylinder</div>
+	<div class="modal-body">
+		<label for="diameter">Diameter: </label><br/>
+		<input type="range" id="diameter" value="%3" min="0.1" max="2.0" step="0.1" oninput="diameterVal.value = this.value" />
+		<output id="diameterVal">%4</output><br/>
+		<label for="height">Height: </label><br/>
+		<input type="range" id="height" value="%1" min="0.1" max="2.0" step="0.1" oninput="heightVal.value = this.value" />
+		<output id="heightVal">%2</output>
+	</div>`
+}
+
+function stringJoin(s: string, r: Array<any>) {
+	r.map((v,i)=>{
+		s = s.replace('%'+(i+1),v)
+	  })
+	return s
+}
 
 function prepareScene() {
+
 	// Camera
 	const camera = new ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 4, new Vector3(0, 0, 0), scene);
 	camera.attachControl(canvas, true);
@@ -59,7 +71,7 @@ function prepareScene() {
 		// We try to pick an object
 		var pickResult = scene.pick(scene.pointerX, scene.pointerY);
 		if (pickResult?.hit) {
-			var currentMesh = pickResult.pickedMesh;
+			currentMesh = pickResult.pickedMesh;
 			console.log(currentMesh?.id);
 			toggleModal();
 			var modalContent = document.getElementById("modal-iframe");
@@ -72,7 +84,7 @@ function prepareScene() {
 						modalContent.innerHTML = meshContent.IcoSphere;
 						return;
 					case "Cylinder":
-						modalContent.innerHTML = meshContent.Cylinder;
+						modalContent.innerHTML = stringJoin(meshContent.Cylinder, [cylinderHeight, cylinderHeight, cylinderDiameter, cylinderDiameter]);
 						return;
 					default:
 						return;
@@ -87,6 +99,18 @@ function prepareScene() {
 prepareScene();
 
 engine.runRenderLoop(() => {
+	if (currentMesh?.id === "Plane") {
+		
+	} else if (currentMesh?.id === "IcoSphere") {
+
+	} else if (currentMesh?.id === "Cylinder") {
+		cylinderHeight = Number((document.getElementById("height") as HTMLInputElement)?.value) || cylinderHeight;
+		cylinderDiameter = Number((document.getElementById("diameter") as HTMLInputElement)?.value) || cylinderDiameter;
+		currentMesh.scaling.y = cylinderHeight / 2;
+		currentMesh.scaling.x = cylinderDiameter / 1;
+		currentMesh.scaling.z = cylinderDiameter / 1;
+	}
+
 	scene.render();
 });
 
